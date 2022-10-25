6b1fbc7883a982d56b5decace6d8478ef8a0c0e0
ewagut <105676327+ewagut@users.noreply.github.com>
2022-10-24 20:36:57 -0500

    Case Study

---
+++ js/index.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    //show popup
+    $("#show-popup-btn").click(function(){
+        $("#popup-container").show();
+    })
+    //close popup
+    $("#close-btn").click(function(){
+        $("#popup-container").hide();
+    })
+})
